d42730b01a8aa38eae21658b2a7880bca7eef199
Stevan Little <stevan.little@gmail.com>
2017-10-16 21:40:23 +0200

    simple roles.pm

---
+++ lib/roles.pm
@@ -0,0 +1,50 @@
+package roles;
+
+use strict;
+use warnings;
+
+use MOP         ();
+use Devel::Hook ();
+
+our $VERSION   = '0.01';
+our $AUTHORITY = 'cpan:STEVAN';
+
+sub import {
+    shift;
+    my $role = caller;
+    my @roles = @_;
+
+    {
+        no strict   'refs';
+        no warnings 'once';
+        push @{ $role.'::DOES' } => @roles;
+    }
+
+    Devel::Hook->push_UNITCHECK_hook(sub {
+        my $meta;
+        {
+            no strict   'refs';
+            no warnings 'once';
+            if ( @{ $role.'::ISA' } ) {
+                $meta = MOP::Class->new( $role )
+            }
+            else {
+                $meta = MOP::Role->new( $role )
+            }
+        }
+
+        MOP::Util::APPLY_ROLES(
+            $meta,
+            [ $meta->roles ],
+            to => ($meta->isa('MOP::Class') ? 'class' : 'role')
+        );
+    });
+}
+
+1;
+
+__END__
+
+=pod
+
+=cut
